fb70d964070630394e310accd4fa1f6beb1c480d
tomhuda <tomhuda@strobecorp.com>
2011-05-21 22:13:48 -0700

    Import updated, minified SproutCore. Include sample Hello World app.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-$(function(){
-  $("#jquery-test").remove();
-});
+var App = SC.Application.create();
 
+App.MyView = SC.View.extend({
+  mouseDown: function() {
+    window.alert("hello world!");
+  }
+});
