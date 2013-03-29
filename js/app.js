913c4f65c986da00961221356cc74c17ac270553
Yehuda Katz <wycats@gmail.com>
2013-03-29 11:10:21 -0700

    Add router stub and clean up IndexRoute

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
-App = Ember.Application.create({});
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  // put your routes here
+});
 
 App.IndexRoute = Ember.Route.extend({
-  setupController: function(controller) {
-    controller.set('content', ['red', 'yellow', 'blue']);
+  model: function() {
+    return ['red', 'yellow', 'blue'];
   }
 });
